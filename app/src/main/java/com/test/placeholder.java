eb3495f618c7364f32f1cafdeeeec587ac6aa455
shlok-k720 <shlokkhandelwal2013@gmail.com>
2026-06-13 09:47:36 +0530

    Revert "Added more libraries"
    This reverts commit 2aede8fb61259a5e28c9e5b339b856da28181094.

--- app/src/main/java/com/test/placeholder.java
+++ app/src/main/java/com/test/placeholder.java
@@ -7,14 +7,9 @@ import com.pedropathing.ftc.drivetrains.Mecanum;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.ftc.localization.localizers.PinpointLocalizer;
-import com.pedropathing.ivy.Command;
-import com.pedropathing.ivy.groups.Groups;
-import com.pedropathing.telemetry.Selectable;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerImpl;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.openftc.easyopencv.OpenCvCamera;
 
 /**
  * This folder is where you would normally put all java/kt classes
@@ -27,11 +22,9 @@ public class placeholder {
     // If this throws an error, try rebuilding the project and syncing gradle files
     // If that doesn't work, try invalidating caches and restarting
 
+    // In order: RobotCore, Hardware, Pedro-FTC, Pedro-Core
     OpMode opMode = new OpModeManagerImpl.DefaultOpMode();
     Servo servo = opMode.hardwareMap.get(Servo.class, "servoName");
     PinpointLocalizer localizer = new PinpointLocalizer(opMode.hardwareMap, new PinpointConstants());
     Follower follower = new Follower(new FollowerConstants(), localizer, new Mecanum(opMode.hardwareMap, new MecanumConstants()));
-    Command command = Groups.parallel(Command.NOOP, Command.NOOP);
-    Selectable<OpMode> selectableOpMode;
-    OpenCvCamera cvCamera;
 }
